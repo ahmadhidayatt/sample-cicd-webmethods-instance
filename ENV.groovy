53a0213b8310c5f72ebdd98a91f514ae368e90a6
ahmadhidayatt <ahmadhidayatt@gmail.com>
2026-09-20 17:11:25 +0700

    feat(cicd): setup production CI/CD on master with DEV target 46.250.226.196, TEST/QA targets, and DeployToDev stage

---
+++ ENV.groovy
@@ -0,0 +1,35 @@
+environments {
+	DEV {
+		IntegrationServers {
+			is_node1 {
+				version = "11.1"
+				host = System.getenv('DEV_IS_HOST') ?: "46.250.226.196"
+				port = System.getenv('DEV_IS_PORT') ?: "5555"
+				username = System.getenv('DEV_IS_USER') ?: "Administrator"
+				pwd = System.getenv('DEV_IS_PWD') ?: "m@n@g3"
+			}
+		}
+	}
+	TEST {
+		IntegrationServers {
+			is_node1 {
+				version = "11.1"
+				host = System.getenv('TEST_IS_HOST') ?: "84.247.147.194"
+				port = System.getenv('TEST_IS_PORT') ?: "5599"
+				username = System.getenv('TEST_IS_USER') ?: "Administrator"
+				pwd = System.getenv('TEST_IS_PWD') ?: "manage"
+			}
+		}
+	}
+	QA {
+		IntegrationServers {
+			is_node1 {
+				version = "11.1"
+				host = System.getenv('QA_IS_HOST') ?: "84.247.147.194"
+				port = System.getenv('QA_IS_PORT') ?: "5599"
+				username = System.getenv('QA_IS_USER') ?: "Administrator"
+				pwd = System.getenv('QA_IS_PWD') ?: "manage"
+			}
+		}
+	}
+}
